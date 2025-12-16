242d78c67f0cd7f09c15b8f14978a0b0ead83ccd
Sheibha-R <sheibha.1143@gmail.com>
2025-12-16 16:36:57 +1100

    made changes

--- 5.3C/services/books.service.js
+++ 5.3C/services/books.service.js
@@ -1,12 +1,26 @@
 const Book = require("../models/book.model");
 
+function normalizePrice(doc) {
+  let price = doc.price;
+
+  // If Mongo returns Decimal128 object, convert it
+  if (price && typeof price === "object") {
+    // Common shape when using Decimal128
+    if (price.$numberDecimal) price = price.$numberDecimal;
+    else if (typeof price.toString === "function") price = price.toString();
+  }
+
+  return { ...doc, price };
+}
+
 async function getAllBooks() {
-  // lean + getters for Decimal128 -> string
-  return Book.find({}).lean({ getters: true });
+  const books = await Book.find({}).lean();
+  return books.map(normalizePrice);
 }
 
 async function getBookById(id) {
-  return Book.findOne({ id }).lean({ getters: true });
+  const book = await Book.findOne({ id }).lean();
+  return book ? normalizePrice(book) : null;
 }
 
 module.exports = {
